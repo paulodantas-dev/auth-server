9e547cfd4a6f9ff24ed1264c049639e182ca1cdd
Paulo Dantas <paulo.roberto@hdn.digital>
2022-07-04 15:23:48 -0300

    Fix vscode

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,11 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 app.use(cookieParser());
 app.use(
   fileUpload({
