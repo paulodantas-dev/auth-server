563c3e9dde1ad41b13caefe9c1253a4893154f28
Paulo Dantas <paulo.roberto@hdn.digital>
2022-07-07 07:36:15 -0300

    try deploy vercel

--- src/server.ts
+++ src/server.ts
@@ -16,11 +16,7 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 app.use(
   fileUpload({
